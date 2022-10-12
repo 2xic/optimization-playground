3bc3ef01a6ac867aed0b33d6fb7d78ff7ce75157
2xic <5640782+2xic@users.noreply.github.com>
2022-10-12 14:07:59 +0200

    Started on the traning loop for muzero

--- rl/muzero/main.py
+++ rl/muzero/main.py
@@ -4,27 +4,30 @@ from model import Model
 import torch
 from optimization_utils.envs.SimpleEnv import SimpleEnv
 from optimization_utils.tree.MonteCarloTreeSearch import MonteCarloTreeSearch
+from optimization_utils.tree.MuzeroSimpleEnvState import MuzeroSimpleEnvState
 from optimization_utils.tree.SimpleEnvState import SimpleEnvState
+from optimization_utils.tree.MuzeroMonteCarloNode import MuzeroMonteCarloNode
 from parameters import ROLLOUT_STEPS_K
 
-def learn(replay_buffer: ReplayBuffer, model: Model):
+def learn(replay_buffer: ReplayBuffer, model: Model, optimizer: torch.optim.Adam):
     #                           <- maybe replay buffer should give trajectory ids :/
     trajectory = replay_buffer.sample_trajectory()
     observation = trajectory[0].state
-    representation = model.get_state_representation(observation)
+
+    representation = model.get_state_representation(observation.reshape((1, -1)).float())
 
     
-    loss = torch.tensor(0)
+    loss = torch.tensor(0).float()
     for index in range(ROLLOUT_STEPS_K):
         trajectory_idx = trajectory[index]
         real_action = trajectory_idx.action
         real_reward = trajectory_idx.reward
 
-        policy = trajectory_idx['metadata'].policy
-        value = trajectory_idx['metadata'].value
+        policy = torch.tensor(trajectory_idx.metadata['policy'])
+        value = torch.tensor(trajectory_idx.metadata['value'])
 
-        predicted_reward, next_state = model.dynamics(representation, real_action)
-        predicted_policy, predicted_value = model.prediction(next_state)
+        predicted_reward, next_state = model.get_dynamics(representation, torch.tensor([real_action]).reshape((1, -1)).float())
+        predicted_policy, predicted_value = model.get_prediction(next_state)
 
         loss_function = lambda x, y: ((x - y) ** 2).sum()
 
@@ -39,44 +42,76 @@ def learn(replay_buffer: ReplayBuffer, model: Model):
             predicted_value
         )
 
-    # pass
-    """
+    
     optimizer.zero_grad()
-    loss.backwards()
+    loss.backward()
     optimizer.step()
-    """
 
+    return loss.item()
         
-def play(replay_buffer: ReplayBuffer, model: Model):
-    env = SimpleEnv()
-    planner = MonteCarloTreeSearch(
-        SimpleEnvState(env)
-    )
-    action = planner.get_action()
-
+def play(replay_buffer: ReplayBuffer, model: Model, epoch_id:int):
+    env = SimpleEnv().reset()
     state = env.state
-    _, reward, _, _ = env.step(action)
+    sum_reward = 0
+    actions = []
+    while not env.done():
+        """
+        TODO: 
+            Dynamics and the reward components should be added here.
+            Need to make some adjustments to the monte carlo logic to make sure it takes
+            into account the new information about the reward.
+        """
+        planner = MonteCarloTreeSearch(
+            MuzeroSimpleEnvState(
+                model.get_state_representation(
+                    torch.tensor(env.state).reshape((1, -1)).float()
+                ),
+                legal_actions=env.legal_actions,
+                action_space=env.action_space,
+                dynamics=model.get_dynamics
+            ),
+            node=MuzeroMonteCarloNode
+        ) 
+        # do the search
+        planner.get_action(iterations=100)
+        # we use muzero
+        action = planner.root.muzero_action
 
-    """
-        Nooo, the replay buffer should contain the entire game history (trajectory)
-    """
-    replay_buffer.push(
-        state=state,
-        reward=reward,
-        action=action
-    )
+        state, reward, _, _ = env.step(action)
 
-    print(action)
-    print(reward)
+        replay_buffer.push(
+            state=state,
+            reward=reward,
+            action=action,
+            metadata={
+                "policy": planner.muzero_policy,
+                "value": planner.muzero_value
+            },
+            id=epoch_id
+        )
+        sum_reward += reward
+        actions.append(action)
 
-timer = StopTimer()
+    return sum_reward, actions
+
+timer = StopTimer(
+    iterations=10_000
+)
 replay_buffer = ReplayBuffer()
-model = Model()
+model = Model(
+    state_size=2,
+    action_space=2
+)
+optimizer = torch.optim.Adam(model.parameters())
 
 timer.tick()
 
 while not timer.is_done():
-    play(replay_buffer, model)
-    learn(replay_buffer, model)
+    reward, actions = play(replay_buffer, model, timer.epoch)
+    loss = learn(replay_buffer, model, optimizer)
+
+    if timer.epoch % 10 == 0:
+        print(timer.epoch, reward, actions, loss)
+
     timer.tick()
-    break
+
--- rl/muzero/model.py
+++ rl/muzero/model.py
@@ -2,12 +2,45 @@ from optimization_utils.tree.State import State
 import random
 import torch
 
-class Model:
-    def __init__(self) -> None:
-        pass
+""" 
+Same component as used in VPN
+"""
+class TinyModel(torch.nn.Module):
+    def __init__(self, input_size, output_size):
+        super(TinyModel, self).__init__()
+        self.l1 = torch.nn.Linear(input_size, 200)
+        self.l2 = torch.nn.Linear(200, output_size)
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.l2(x)
+        return x
+
+class Model(torch.nn.Module):
+    def __init__(self, state_size, action_space) -> None:
+        super(Model, self).__init__()
+
+        self.state_representation_size = 4
+        self.action_space = action_space
+
+        self.representation = TinyModel(state_size, self.state_representation_size)
+        self.dynamics = TinyModel(self.state_representation_size + 1, self.state_representation_size + 1)
+        self.prediction = TinyModel(self.state_representation_size, action_space + 1)
 
     def get_action(self, state: State):
         return random.sample(state.possible_actions)
 
     def get_state_representation(self, state: torch.Tensor):
-        return state
+        return self.representation(state)
+
+    def get_dynamics(self, state: torch.Tensor, action: int):
+        combined = torch.concat([state, action], dim=1)
+        results = self.dynamics(combined)
+        return results[:, 0], results[:, 1:]
+
+    def get_prediction(self, state: torch.Tensor):
+        policy_reward = self.prediction(state)
+
+        return policy_reward[:, :self.action_space], policy_reward[:, -1]
+
