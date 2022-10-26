b2a6d97cbaa762c92ea53239a91cf43ef53c5b56
2xic <5640782+2xic@users.noreply.github.com>
2022-10-26 20:58:10 +0200

    More work on the SAC model with openai gym

--- rl/diviersity-is-all-you-need/soft-actor-critic/main.py
+++ rl/diviersity-is-all-you-need/soft-actor-critic/main.py
@@ -1,3 +1,4 @@
+from functools import total_ordering
 import torch
 from optimization_utils.envs.SimpleEnv import SimpleEnv
 from optimization_utils.exploration.EpsilonGreedyManual import EpsilonGreedyManual
@@ -5,48 +6,64 @@ import random
 from optimization_utils.diagnostics.Diagnostics import Diagnostics
 from model import SAC
 from optimization_utils.replay_buffers.ReplayBuffer import ReplayBuffer
+import gym
 
-env = SimpleEnv()
+env =  gym.make('CartPole-v0') #SimpleEnv()
 replay_buffer = ReplayBuffer()
 
 def play(model: SAC, diagnostics: Diagnostics):
     global env, replay_buffer
-    env.reset()
+    env.reset() 
 
-    while not env.done():
-        action_distribution = model(env.env.float())
-        #print(action_distribution)
-        action = epsilon.get_action(lambda: torch.argmax(action_distribution).item())
-
-        (_, reward, action, _) = env.step(action)
+    total_reward = 0
+    done = False
+    while not done: # env.done():
+        prev_state = torch.tensor(list(env.state)).clone() # torch.from_numpy(env.state)
+        action, _ = model.action(prev_state)
+        action = action.item()
+#        print(action)
+     #   print(env.step(action))
+        (_, reward, done, _, _) = env.step(action)
         total_reward += reward
 
-        game.add(action_distribution, action)
-        diagnostics.profile(action)
+    #    print(env.state)
+
+        diagnostics.profile(int(action))
+        diagnostics.reward(reward)
         replay_buffer.push(
-            state=state,
+            state=prev_state,
             reward=reward,
             action=action,
+            is_last_state=done, #env.done(),
             metadata={
-                "policy": planner.muzero_policy,
-                "value": planner.muzero_value
+                "next_state": torch.tensor(list(env.state)).clone(),
             },
-            id=epoch_id
+            id=-1
         )
-    exit(0)
+    return total_reward
 
 if __name__ == "__main__":
     model = SAC(
-        input_size=env.state_size,
-        output_size=env.action_space,
+        input_size=4, #env.state_size,
+        output_size=2, #env.action_space,
     )
     diagnostics = Diagnostics()
 
     for epoch in range(10_000):
-        loss = play(model, diagnostics)
+        total_reward = play(model, diagnostics)
 
-        if epoch % 100 == 0:
-            diagnostics.print(epoch, metadata={
-                "loss": loss.item() 
-            })
+        if epoch % 100 == 0 and epoch > 0:
+            #for i in range(100):
+            metadata = model.optimize(replay_buffer)
+            metadata['total_reward'] = total_reward
 
+            diagnostics.print(epoch, metadata=metadata)
+            """
+            metadata = {}
+            metadata["policy_view_zero_state"] = str(model.action(torch.tensor([[1, 0]]).float())[1].data)
+            metadata["policy_view_one_state"] = str(model.action(torch.tensor([[0, 1]]).float())[1].data)
+
+            metadata["q_view_zero_state"] = str(model.q_1(torch.tensor([[1, 0]]).float()).data)
+            metadata["q_view_one_state"] = str(model.q_2(torch.tensor([[0, 1]]).float()).data)
+            diagnostics.model_print(metadata=metadata)
+            """
--- rl/diviersity-is-all-you-need/soft-actor-critic/model.py
+++ rl/diviersity-is-all-you-need/soft-actor-critic/model.py
@@ -1,5 +1,10 @@
+from cmath import polar
+from email import policy
 from math import nextafter
+from xml.sax.xmlreader import InputSource
 import torch
+from optimization_utils.replay_buffers.ReplayBuffer import ReplayBuffer
+
 
 class SimpleModel(torch.nn.Module):
     def __init__(self, input_size, output_size):
@@ -9,61 +14,163 @@ class SimpleModel(torch.nn.Module):
         self.l3 = torch.nn.Linear(256, output_size)
 
     def forward(self, x):
+        assert len(x.shape) == 2, f"wrong shape, got {x.shape}"
+
         x = torch.nn.functional.relu(self.l1(x))
-        x = torch.nn.functional.relu(self.l2(x))
+        x = torch.nn.functional.sigmoid(self.l2(x))
         x = torch.nn.functional.sigmoid(self.l3(x))
+#        x = self.l3(x)
+
         return x
 
+
+class PolicyNetwork(torch.nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(PolicyNetwork, self).__init__()
+    
+        self.linear1 = torch.nn.Linear(num_inputs, 128)
+        self.linear2 = torch.nn.Linear(128, 128)
+
+        self.mean_linear = torch.nn.Linear(128, num_actions)
+        self.log_std_linear = torch.nn.Linear(128, num_actions)
+
+    def forward(self, state):
+        self.log_std_min = -2
+        self.log_std_max = 2
+
+        x = torch.nn.functional.relu(self.linear1(state))
+        x = torch.nn.functional.relu(self.linear2(x))
+
+        mean = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+
+        return mean, log_std
+
+    def get_action(self, state):
+        (mean, log_std) = self.forward(state)
+        std = log_std.exp()
+
+        normal = torch.distributions.Normal(0, 1)
+        z = normal.sample()
+        action = torch.tanh(mean + std*z)
+        action = action.cpu()
+        return action
 
 
 class SAC:
     def __init__(self, input_size, output_size):
-        self.action_network = SimpleModel( input_size, output_size) 
-        self.q_1 = SimpleModel( input_size, output_size)
-        self.q_2 = SimpleModel( input_size, output_size)
+        self.action_network = PolicyNetwork(input_size, output_size)
+        self.q_1 = SimpleModel(input_size, output_size)
+        self.q_2 = SimpleModel(input_size, output_size)
 
-        self.q_1_target = SimpleModel( input_size, output_size)
-        self.q_2_target = SimpleModel( input_size, output_size)
-        self.update()
-        self.optimizer = torch.optim.Adam()
-        self.gamma = 1
-        self.alpha = 0.1
+        self.q_1_target = SimpleModel(input_size, output_size)
+        self.q_2_target = SimpleModel(input_size, output_size)
+        self.update_target(tau=1)
 
-    def optimize(self, replay):
+        lr = 1e-4
+        self.optimizer_q_1 = torch.optim.Adam(self.q_1.parameters(), lr=lr)
+        self.optimizer_q_2 = torch.optim.Adam(self.q_2.parameters(), lr=lr)
+        self.optimizer_action = torch.optim.Adam(
+            self.action_network.parameters(), lr=lr)
+        self.gamma = 0.7
+        self.alpha = 0.4
+
+    def optimize(self, replay: ReplayBuffer):
         # TODO: sample this from replay
-        state, reward, next_state, is_last_state = None , None, None, None
 
-        # update q
-        for i in [self.q_1, self.q2]:
-            network_value = i(state) 
-            target_q = self.y(reward, next_state, is_last_state)
-            error = (network_value - target_q) ** 2
-            # TODO: backward
-        # update policy
-    
-        pass
+        self.optimizer_action.zero_grad()
+        self.optimizer_q_1.zero_grad()
+        self.optimizer_q_2.zero_grad()
+
+        total_error_policy = torch.zeros(1)
+        total_error_q = torch.zeros(1)
+        for _ in range(256):
+            event = replay.sample()
+            state, reward, next_state, is_last_state = event.state, event.reward, event.metadata[
+                'next_state'], event.is_last_state
+            next_state = self.reshape_tensor(next_state)
+            state = self.reshape_tensor(state)
+
+            # update q
+            for model, optimizer in zip([self.q_1, self.q_2], [self.optimizer_q_1, self.optimizer_q_2]):
+                model.zero_grad()
+                state = state.float()
+                next_state = next_state.float()
+
+                network_value = model(state)
+                target_q = None
+                with torch.no_grad():
+                    target_q = self.y(reward, next_state, is_last_state)
+
+                assert not torch.isnan(target_q)
+                assert not torch.any(network_value == torch.nan)
+                assert not torch.any(target_q == torch.nan)
+
+                error = ((network_value - target_q) ** 2).sum()
+                error.backward()
+                # optimizer.step()
+                total_error_q += error
+
+            # update policy
+            error = self.q_policy_error(state, use_target=False)
+            error.backward()
+            total_error_policy += error
+
+        self.optimizer_action.step()
+        self.optimizer_q_1.step()
+        self.optimizer_q_2.step()
+
+        self.update_target()
+
+        return {
+            "total_error_policy": total_error_policy.item(),
+            "total_error_q": total_error_q.item(),
+        }
+
+    def q_policy_error(self, next_state, use_target):
+        next_state = self.reshape_tensor(next_state)
+        min_q, policy = self.min_q(next_state, use_target)
+        assert not torch.isnan(min_q)
+        assert not torch.isnan(policy), policy
+        return min_q - self.alpha * policy
 
     def y(self, reward, next_state, is_last_state):
-        min_q, policy = self.min_q(next_state)
+        next_state = self.reshape_tensor(next_state)
         return reward + self.gamma * (
             1 - int(is_last_state)
-        ) * min_q \
-        - self.alpha * policy
-    
-    def min_q(self, next_state):
-        next_action = self.q_1(next_state)
-        action = torch.argmax(next_action)
-        return torch.min(
-            self.q_1_target(next_state)[action],
-            self.q_1_target(next_state)[action]
-        ), next_action[action]
+        ) * self.q_policy_error(next_state, use_target=True)
+
+    def min_q(self, next_state, use_target):
+        next_state = self.reshape_tensor(next_state)
+        action, policy = self.action(next_state)
+        min_q = None
+        with torch.no_grad():
+            min_q = torch.min(
+                self.q_1_target(next_state)[0][action] if use_target else self.q_1(
+                    next_state)[0][action],
+                self.q_2_target(next_state)[0][action] if use_target else self.q_2(
+                    next_state)[0][action]
+            )
+
+        return min_q, (policy[0][action])
 
     def action(self, state):
-        return self.q_1(state)
+        state = self.reshape_tensor(state)
+        policy = self.action_network.get_action(state.float())
+#        print(policy.shape)
+#        exit(0)
+        # torch.distributions.Categorical(policy).sample()
+        action = torch.argmax(policy, dim=1)
+        return action, policy
 
-    def update(self):
-        self.q_1_target.load_state_dict(self.q_1.state_dict())
-        self.q_2_target.load_state_dict(self.q_2.state_dict())
+    def update_target(self, tau=0.4):
+        for target, local in zip([self.q_1_target, self.q_2_target], [self.q_1, self.q_2]):
+            for target_param, local_param in zip(target.parameters(), local.parameters()):
+                target_param.data.copy_(
+                    tau*local_param.data + (1.0-tau)*target_param.data)
 
-    def parameters(self):
-        return self.q_1.parameters()
+    def reshape_tensor(self, x):
+        if len(x.shape) == 1:
+            return x.reshape((1, ) + x.shape)
+        return x
