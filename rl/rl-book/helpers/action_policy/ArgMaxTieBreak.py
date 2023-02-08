Fixed bug in average plots for td

rl/rl-book/helpers/action_policy/ArgMaxTieBreak.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-def argmax_tie_break(numpy_array):
+def argmax_tie_break(numpy_array, non_max=None):
+    if non_max is not None:
+        for index in range(len(numpy_array)):
+            if index not in non_max:
+                numpy_array[index] = float('-inf')
+
     return np.random.choice(np.where(numpy_array == numpy_array.max())[0])

rl/rl-book/temporal-difference-learning/expected_sarsa.py
@@ -11,6 +11,8 @@ from helpers.play_tic_tac_toe_vs_random_agent import play_tic_tac_toe
 from helpers.State import State
 import os
 
+from helpers.action_policy.ArgMaxTieBreak import argmax_tie_break
+import numpy as np
 
 class ExpectedSarsa:
     def __init__(self, action) -> None:
@@ -33,8 +35,10 @@ class ExpectedSarsa:
         return self.softmax(self.q[str(self.env)].np(), legal_actions=self.env.legal_actions)
 
     def get_action(self):
-        action = self.epsilon(self)
-        return action
+        return argmax_tie_break(
+            self.q[str(self.env.state)].np().astype(np.float),
+            non_max=self.env.legal_actions,
+        )
 
     def train(self, env: TicTacToe):
         self.env = env

rl/rl-book/temporal-difference-learning/q_learning.py
@@ -32,13 +32,11 @@ class Q_learning:
         return random.sample(self.env.legal_actions, k=1)[0]
 
     def on_policy(self):
-        p = self.q[str(self.env.state)].np().astype(np.float)
-        for index in range(len(p)):
-            if index not in self.env.legal_actions:
-                p[index] = float('-inf')
         return argmax_tie_break(
-            p
+            self.q[str(self.env.state)].np().astype(np.float),
+            non_max=self.env.legal_actions,
         )
+
         #return (self.q[str(self.env.state)].np()).argmax()
         return self.softmax(
             self.q[str(self.env.state)].np(), 