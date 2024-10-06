Found off by one bug

nlp/torch-transformer/train_gpt_small_document_debug.py
@@ -36,12 +36,13 @@ for _ in range(1024):
     accuracy = 0
     sum_loss = 0
 #    for i in range(X.shape[0]):
-    output = model(X)
+    output = model(X).reshape((-1, config.vocab_size))
     loss = torch.nn.functional.cross_entropy(
-        output.reshape((output.shape[0], -1)),
+        output,
         y.reshape((-1)),
         ignore_index=-1
     )
+    #print(output)
     accuracy += (
         torch.argmax(output, dim=1) == y.reshape((-1))
     ).sum()
@@ -53,17 +54,15 @@ for _ in range(1024):
     optimi.step()
     accuracy_pct = accuracy / (y.shape[0] * y.shape[1]) * 100
     print(accuracy_pct, sum_loss, count_full_win)
-#    raw = model.rollout(
-#        source_vocab.encode("hello"), 
-#        2,
-#        sampling="argmax"
-#    )
     raw = torch.argmax(
-        model.raw_forward(X)[:, ::3, :],
+        model(X[:1]).reshape((-1, config.vocab_size)),
         dim=1
-    ).tolist()
-    print(raw)
-    print(source_vocab.decode(raw))
+    ).tolist()[::config.sequence_length]
+    tokens, x_tokens = model.rollout(source_vocab.encode("hello"), 32)
+    print(X[:1])
+    print(x_tokens[:1])
+    print("Rollout:\n\t", source_vocab.decode(tokens))
+    print("Forward:\n\t", source_vocab.decode(raw))
     print("")
     if int(accuracy_pct) > 99:
         count_full_win += 1

shared-library/src/optimization_playground_shared/nlp/GptTransformer.py
@@ -88,9 +88,10 @@ class GptTransformerModel(nn.Module):
     def forward(self, x: Tensor):
         results = self.raw_forward(x)
         # (batch size, sequence size, vocab_size)
-        reshaped = results.view(-1, self.config.vocab_size)
-
-        return reshaped
+        #print(results.shape)
+        #reshaped = results.view(-1, self.config.vocab_size)
+        #print(reshaped.shape)
+        return results
 
     def embeddings(self, X):
         with torch.no_grad():
@@ -132,6 +133,7 @@ class GptTransformerModel(nn.Module):
         return next(self.parameters()).device
 
     def rollout(self, seed, steps, sampling="argmax"):
+        X_tensors = []
         with torch.no_grad():
             output = seed
             for _ in range(steps):
@@ -139,9 +141,9 @@ class GptTransformerModel(nn.Module):
                 context_tensor = torch.tensor(output[-self.sequence_size:]).long()
                 X[0, :context_tensor.shape[0]] = context_tensor
                 next_predicted_token = self.raw_forward(X)
-                next_predicted_token = next_predicted_token[:, 2 ,:]
+                next_predicted_token = next_predicted_token[:, ::self.config.sequence_length ,:][0]
                 next_predicted_token = torch.nn.functional.softmax(next_predicted_token, dim=1)
-                print("output ", next_predicted_token)
+              #  print("output ", next_predicted_token)
                 samplings = {
                     "argmax": argmax_sampling,
                     "temperature": temperature_sampling
@@ -149,44 +151,7 @@ class GptTransformerModel(nn.Module):
                 next_predicted_token = samplings[sampling](
                     next_predicted_token
                 ).item()
-           #     print(next_predicted_token)
                 assert type(next_predicted_token) == int, next_predicted_token
                 output.append(next_predicted_token)
-
-            """
-            prev = None
-            for index in range(steps):
-                next_predicted = None
-                if len(seed) <= index:
-                    X = torch.full((1, self.sequence_size), self.config.padding_index).reshape(1, -1).to(self.device).long()
-                    context_tensor = torch.tensor(output[-self.sequence_size:]).long()
-                    X[0, :context_tensor.shape[0]] = context_tensor
-                    assert prev is None or torch.all(prev[0, 1:] == X[0, :-1])
-
-                    next_token_location = (
-                        -1 if context_tensor.shape[0] == self.sequence_size else context_tensor.shape[0] - 1
-                    )
-                    next_token = self.predict(X, next_token_location=next_token_location)
-                    next_predicted = None
-
-                    if sampling == "argmax":
-                        next_predicted = argmax_sampling(
-                            next_token
-                        ).item()
-                    else:
-                        next_predicted = temperature_sampling(
-                            next_token[0]
-                        ).item()
-                    assert type(next_predicted) == int, next_predicted
-                    output.append(next_predicted)
-                    prev = X
-                else:
-                    if torch.is_tensor(seed[index]):
-                        next_predicted = seed[index].item()
-                    else:
-                        next_predicted = seed[index]
-                    assert type(next_predicted) == int, next_predicted
-                    output.append(next_predicted)
-            return output
-            """
-        return output
+                X_tensors.append(X)
+        return output, X_tensors